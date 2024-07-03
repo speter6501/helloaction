5e2a5003a065a0368af2c9fb29588d0a1ab628ad
speter6501 <speter650@gmail.com>
2024-07-03 11:29:00 +0900

    second commit

---
+++ src/test/java/com/ohgiraffers/dockerimagetest/DockerImageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.dockerimagetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
